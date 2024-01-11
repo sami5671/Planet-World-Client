18be3e0c1c2cc28d35675a6effd418b588e02dd7
sami5671 <samialam5671@gmail.com>
2024-01-11 22:52:06 +0600

    designed the who we are section

--- src/Components/SectionTitle.jsx
+++ src/Components/SectionTitle.jsx
@@ -3,9 +3,9 @@ import { GiVikingChurch } from "react-icons/gi";
 const SectionTitle = ({ subHeading, heading }) => {
   return (
     <div className="mx-auto text-center font-Rancho md: w-4/12 my-8">
-      <GiVikingChurch className="mx-auto text-2xl text-orange-300" />
-      <p className="text-orange-300 mb-2">-----{subHeading}-----</p>
-      <p className="text-3xl text-amber-400 uppercase border-y-2 border-orange-200 py-2">
+      <GiVikingChurch className="mx-auto text-3xl text-lime-800" />
+      <p className="text-lime-600 mb-2"> ----- {subHeading} ----- </p>
+      <p className="text-3xl text-lime-500 uppercase border-y-2 border-lime-500 py-2">
         {heading}
       </p>
     </div>
--- src/Home/Home/Home.jsx
+++ src/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../Banner/Banner";
+import WhoWeAre from "./WhoWeAre";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <WhoWeAre></WhoWeAre>
     </div>
   );
 };
